342a6afa620b0feb6e3ccde27b55d8f38c20e638
Talla2XLC <mmm-appp@mail.ru>
2025-04-08 23:13:42 +0300

    Corrected file name

---
+++ go_sum.go
@@ -0,0 +1,5 @@
+package go_sum
+
+func Add(a int, b int) int {
+	return a + b
+}
--- main.go
+++
@@ -1,8 +0,0 @@
-package go_sum
-
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
-func Add(a int, b int) int {
-	return a + b
-}
